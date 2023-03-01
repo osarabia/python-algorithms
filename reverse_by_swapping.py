4165b474793a6da4bf099cf2804d9a3a82a288d8
Omar Sarabia <elsarabia@hotmail.com>
2023-03-01 15:03:13 -0700

    updated: reverse by swapping unit test

--- reverse_by_swapping.py
+++ reverse_by_swapping.py
@@ -1,11 +1,40 @@
+import unittest
+
+def reverse_by_swapping(array):
+    i = 0
+    while i < len(array)/2:
+        last_index = len(array) - 1 - i
+        first_element = array[i]
+        array[i] = array[last_index]
+        array[last_index] = first_element
+        i = i + 1
+
+    return array
 
 
-array_input = [3,4,5,6,7,8]
-i = 0
-while i < len(array_input)/2:
-    last_index = len(array_input) - 1 - i
-    first_element = array_input[i]
-    array_input[i] = array_input[last_index]
-    array_input[last_index] = first_element
-    i = i + 1
-print(array_input)
+class TestReververBySwapping(unittest.TestCase):
+    def test_odd_length(self):
+        #arrange
+        input_arr = [1,2,3]
+        expected_arr = [3,2,1]
+
+        #action
+        output = reverse_by_swapping(input_arr)
+
+        #assert
+        assert output == expected_arr, "expecting {}, got {}".format(expected_arr, output)
+
+    def test_even_length(self):
+        #arrange
+        input_arr = [1,2,3,4]
+        expected_arr = [4,3,2,1]
+
+        #action
+        output = reverse_by_swapping(input_arr)
+
+        #assert
+        assert output == expected_arr, "expecting {}, got {}".format(expected_arr, output)
+
+
+if __name__ == '__main__':
+    unittest.main()
